22c981425b15fae6b9e7e6f2ca24351415c9c98f
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2025-10-31 12:57:04 -0400

    updates

--- main.js
+++ main.js
@@ -9,15 +9,16 @@ console.log(`global: ${z}`);
 function myFunc() {
   var x = 10;
   const z = 5;
-  console.log(`function: ${x}`);
-  console.log(`function: ${y}`);
-  console.log(`function: ${z}`);
-
   {
+    var x = 11; // function scoped
+    const z = 6; // block scoped
     console.log(`block: ${x}`);
     console.log(`block: ${y}`);
     console.log(`block: ${z}`);
   }
+  console.log(`function: ${x}`);
+  console.log(`function: ${y}`);
+  console.log(`function: ${z}`);
 }
 
 myFunc();
