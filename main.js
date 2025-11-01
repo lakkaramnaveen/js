439c1e25f67fd47feea338f44bc934d196d600a4
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2025-11-01 13:53:45 -0400

    objects

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-// Arrays
+// Objects
+// key-value pairs
+const myObj = { name: "Nai" };
+const anotherObj = {
+  alive: true,
+  answer: 42,
+  hobbies: ["Eat", "Sleep", "Code"],
+  beverage: {
+    morning: "Coffee",
+    afternoon: "Iced tea",
+  },
+};
+console.log(anotherObj.beverage.morning);
