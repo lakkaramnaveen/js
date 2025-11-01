b4dd6af805e21cef1243dbed3319e9d3524d8052
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2025-10-31 20:29:53 -0400

    slice

--- main.js
+++ main.js
@@ -1,15 +1,5 @@
 // Arrays
-const myArray = [];
+const myArray = ["A", "B", "C", "D", "E", "F"];
+const newArray = myArray.slice(2);
 
-// add elements to an array
-myArray[0] = "Dave";
-myArray[1] = 1001;
-myArray[2] = false;
-
-myArray.push("school");
-
-console.log(myArray);
-
-myArray.pop();
-
-console.log(myArray);
+console.log(newArray);
