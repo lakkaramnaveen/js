aef27f672ee95e92837748f2fcd01d06b4091d21
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2025-11-03 10:16:36 -0500

    custom error

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 "use strict";
 const name = () => {
   try {
-    const name = "Nani";
-    name = "joe";
+    throw new customError("this is a custom eeeor");
+    // throw new nameError("hi1");
   } catch (error) {
-    console.warn(error);
+    console.error(error);
   }
 };
 
 name();
+
+// custom error
+
+function customError(message) {
+  this.message = message;
+  this.name = "customError";
+  this.stack = `${this.name} : ${this.message}`;
+}
+
+// function nameError(message) {
+//   console.log("nani");
+// }
