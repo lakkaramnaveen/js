315c720853a87cf55157ad4961fc66ac6a4c8aff
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2025-11-03 15:02:54 -0500

    styling, nav, divs

--- main.js
+++ main.js
@@ -10,5 +10,29 @@ view2.style.display = "none";
 
 const views = document.getElementsByClassName("view");
 console.log(views);
+
 const sameViews = document.querySelectorAll(".view");
 console.log(sameViews);
+
+const divs = view1.querySelectorAll("div");
+console.log(divs);
+
+const sameDivs = view1.getElementsByTagName("div");
+console.log(sameDivs);
+
+const evenDivs = view1.querySelectorAll("div:nth-of-type(2n)");
+console.log(evenDivs);
+
+for (let i = 0; i < evenDivs.length; i++) {
+  evenDivs[i].style.backgroundColor = "darkblue";
+  // evenDivs[i].style.width = "200px";
+  // evenDivs[i].style.height = "200px";
+}
+
+const navText = document.querySelector("nav h1");
+console.log(navText);
+navText.textContent = "Hello world!";
+const navBar = document.querySelector("nav");
+navBar.innerHTML = `<h1>Hello</h1> <p>This should aligh right.</p>`;
+console.log(navBar);
+navBar.style.justifyContent = "space-between";
