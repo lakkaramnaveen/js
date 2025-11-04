7544d249f320c8c8ba3b490c1a0048c6a44566e6
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2025-11-04 12:03:12 -0500

    event listener, ready state

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
-//
-const view = document.querySelector("#view2");
-const div = view.querySelector("div");
-const h2 = div.querySelector("h2");
-
 // add event listener
 
-const doSomething = () => {
-  alert("doing something");
-};
-
-h2.addEventListener("click", doSomething, false);
-h2.removeEventListener("click", doSomething, false);
-
-h2.addEventListener("click", (event) => {
-  console.log(event.target);
-  event.target.textContent = "Clicked";
+document.addEventListener("readystatechange", (event) => {
+  if (event.target.readyState === "complete") {
+    console.log("readyState: complete");
+    initApp();
+  }
 });
+
+const initApp = () => {
+  const view = document.querySelector("#view2");
+  const div = view.querySelector("div");
+  const h2 = div.querySelector("h2");
+
+  view.addEventListener("click", (event) => {
+    view.style.backgroundColor = "purple";
+  });
+
+  div.addEventListener("click", (event) => {
+    view.style.backgroundColor = "blue";
+  });
+
+  h2.addEventListener("click", (event) => {
+    event.target.textContent = "Clicked";
+  });
+};
