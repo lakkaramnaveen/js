8caeb2efeb0a120d986502a2963d390452881054
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2025-11-04 11:53:23 -0500

    event listener

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 //
-const view = document.querySelector("view2");
-console.log(view    );
+const view = document.querySelector("#view2");
+const div = view.querySelector("div");
+const h2 = div.querySelector("h2");
+
+// add event listener
+
+const doSomething = () => {
+  alert("doing something");
+};
+
+h2.addEventListener("click", doSomething, false);
+h2.removeEventListener("click", doSomething, false);
+
+h2.addEventListener("click", (event) => {
+  console.log(event.target);
+  event.target.textContent = "Clicked";
+});
