1057e47d07bf8368f10a7160597e8ee266ada81c
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2025-11-12 14:37:53 -0500

    length of a string

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-const obj = {
-  // __proto__
-  __proto__: theProtoObj,
-  // Shorthand for 'handler: handler'
-  handler,
-  // Methods
-  toString() {
-    // Super calls
-    return `d ${super.toString()}`;
-  },
-  // Computed (dynamic) property names
-  ["prop_" + (() => 42)()]: 42,
-};
+// Will print the number of symbols in the string including whitespace.
+console.log("Joyo's cat".length); // In this case, 10.
+
+// Basic literal string creation
+`In JavaScript '\n' is a line-feed.`;
+
+// Multiline strings
+`In JavaScript, template strings can run
+ over multiple lines, but double and single
+ quoted strings cannot.`;
+
+// String interpolation
+const name = "Lev",
+  time = "today";
+`Hello ${name}, how are you ${time}?`;
