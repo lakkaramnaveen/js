3ab74bbc6bddcb3a4e11bc673a5d28005a4e999d
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2025-11-12 10:04:42 -0500

    data types and conversions

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-const MY_ARRAY = ["HTML", "CSS"];
-MY_ARRAY.push("JAVASCRIPT");
-console.log(MY_ARRAY); // ['HTML', 'CSS', 'JAVASCRIPT'];
+let answer = 12;
+answer = "Thanks for all the fish!";
+console.log(answer);
+
+x = "The answer is " + 42; // "The answer is 42"
+y = 42 + " is the answer"; // "42 is the answer"
+z = "37" + 7; // "377"
+
+console.log(x);
+console.log(y);
+console.log(z);
+
+console.log("37" - 7); // 30
+console.log("37" * 7); // 259
