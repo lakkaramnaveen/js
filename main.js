7ed718b09912bee1ce09149fd2a15f1e41f37c2b
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2025-11-12 10:05:55 -0500

    parse

--- main.js
+++ main.js
@@ -1,14 +1,4 @@
-let answer = 12;
-answer = "Thanks for all the fish!";
-console.log(answer);
-
-x = "The answer is " + 42; // "The answer is 42"
-y = 42 + " is the answer"; // "42 is the answer"
-z = "37" + 7; // "377"
-
-console.log(x);
-console.log(y);
-console.log(z);
-
-console.log("37" - 7); // 30
-console.log("37" * 7); // 259
+let s = "2.121";
+console.log(parseInt(s));
+console.log(parseFloat(s));
+console.log(Number(s));
