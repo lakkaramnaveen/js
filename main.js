2b6670c956081599baff57d63377ff0f18e69116
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2025-11-13 14:33:19 -0500

    broken string

--- main.js
+++ main.js
@@ -1,47 +1,12 @@
-const formatArg = (arg) => {
-  if (Array.isArray(arg)) {
-    // Print a bulleted list
-    return arg.map((part) => `- ${part}`).join("\n");
-  }
-  if (arg.toString === Object.prototype.toString) {
-    // This object will be serialized to "[object Object]".
-    // Let's print something nicer.
-    return JSON.stringify(arg);
-  }
-  return arg;
-};
+const quote = 'He read "The Cremation of Sam McGee" by R.W. Service.';
+console.log(quote);
 
-const print = (segments, ...args) => {
-  // For any well-formed template literal, there will always be N args and
-  // (N+1) string segments.
-  let message = segments[0];
-  segments.slice(1).forEach((segment, index) => {
-    message += formatArg(args[index]) + segment;
-  });
-  console.log(message);
-};
+const home = "c:\\temp";
+console.log(home);
 
-const todos = [
-  "Learn JavaScript",
-  "Learn Web APIs",
-  "Set up my website",
-  "Profit!",
-];
-
-const progress = { javascript: 20, html: 50, css: 10 };
-
-print`I need to do:
-${todos}
-My current progress is: ${progress}
-`;
-
-// I need to do:
-// - Learn JavaScript
-// - Learn Web APIs
-// - Set up my website
-// - Profit!
-// My current progress is: {"javascript":20,"html":50,"css":10}
-
-print(["I need to do:\n", "\nMy current progress is: ", "\n"], todos, progress);
-
-console.log("I need to do:\n%o\nMy current progress is: %o\n", todos, progress);
+const str =
+  "this string \
+is broken \
+across multiple \
+lines.";
+console.log(str); // this string is broken across multiple lines.
