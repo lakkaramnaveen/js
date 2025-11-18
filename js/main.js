5a737f30b7a110d88de09b6b74259fd226edbc34
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2025-11-18 08:47:35 -0500

    modules done

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 import playGuitar from "./guitar.js";
 import { shredding as shred, plucking } from "./guitar.js";
+import User from "./user.js";
+
+const me = new User("email@gmail.com", "nani");
+console.log(me);
+console.log(me.greeting());
 
 console.log(playGuitar());
 console.log(shred());
