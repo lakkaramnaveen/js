9eb0715304da2555cf5885ca8477719219ad6c06
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2025-11-19 22:36:24 -0500

    returned

--- js/main.js
+++ js/main.js
@@ -1,22 +1,11 @@
-const myPromise = new Promise((resolve, reject) => {
-  const error = false;
-  if (!error) {
-    resolve("Yes! resolved the promise!");
-  } else {
-    reject("No! rejected the promise");
-  }
-});
+const users = fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    data.forEach((element) => {
+      console.log(element);
+    });
+  });
 
-const myNextPromise = new Promise((resolve, reject) => {
-  setTimeout(function () {
-    resolve("myNext promies resolved!");
-  }, 0);
-});
-
-myNextPromise.then((value) => {
-  console.log(value);
-});
-
-myPromise.then((value) => {
-  console.log(value);
-});
+console.log(users);
