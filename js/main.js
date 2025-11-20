c13dfc9170fdd881f1320f62872189d29f10c3d9
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2025-11-19 22:17:38 -0500

    resolve and reject, catch

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const myPromise = new Promise((resolve, reject) => {
-  const error = false;
+  const error = true;
   if (!error) {
     resolve("Yes! resolved the promise!");
   } else {
@@ -8,3 +8,14 @@ const myPromise = new Promise((resolve, reject) => {
 });
 
 console.log(myPromise);
+
+myPromise
+  .then((value) => {
+    return value + 1;
+  })
+  .then((newValue) => {
+    console.log(newValue);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
