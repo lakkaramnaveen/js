599d3dbe058895b819ca8097db583624648394db
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2025-11-21 18:32:21 -0500

    fetch, request

--- js/main.js
+++ js/main.js
@@ -1,19 +1,11 @@
-// second parameter of fetch
 
-const joke = {
-  id: "NJBl3TnrHlb",
-  joke: "The other day, my wife asked me to pass her lipsti… her a glue stick. She still isn't talking to me.",
-};
-const postData = async (joke) => {
-  const response = await fetch("https://httpbin.org/post", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(joke),
-  });
+
+const requestJoke = async (firstName, lastName) => {
+  const response = await fetch(
+    `http://api.chucknorris.io/jokes/random?firstName=${firstName}&lastName=${lastName}`
+  );
   const jsonresponse = await response.json();
-  console.log(jsonresponse);
+  console.log(jsonresponse.value);
 };
 
-postData(joke);
+requestJoke("nani", "chuck");
