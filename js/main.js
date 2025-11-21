f928b49e56e827438eb79c9b4097643ea57fbe1f
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2025-11-21 18:41:21 -0500

    forms, procedural program

--- js/main.js
+++ js/main.js
@@ -1,11 +1,33 @@
+const getDataFrom = () => {
+  const requestObj = {
+    firstName: "Bruce",
+    lastName: "Lee",
+    categories: ["nerdy"],
+  };
 
-
-const requestJoke = async (firstName, lastName) => {
-  const response = await fetch(
-    `http://api.chucknorris.io/jokes/random?firstName=${firstName}&lastName=${lastName}`
-  );
-  const jsonresponse = await response.json();
-  console.log(jsonresponse.value);
+  return requestObj;
 };
 
-requestJoke("nani", "chuck");
+const buildReqUrl = (requestData) => {
+  return `http://api.chucknorris.io/jokes/random?firstName=${requestData.firstName}&lastName=${requestData.lastName}`;
+};
+
+const requestJoke = async (url) => {
+  const response = await fetch(url);
+  const jsonResponse = await response.json();
+  const joke = jsonResponse.value.joke;
+  postJokeToPage(joke);
+};
+
+const postJokeToPage = (joke) => {
+  console.log(joke);
+};
+
+const processJoke = async () => {
+  const requestData = getDataFrom();
+  const requstUrl = buildReqUrl(requestData);
+  await requestJoke(requstUrl);
+  console.log("finished!");
+};
+
+processJoke();
