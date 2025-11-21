013455d7d7b532819818d760bcf9e3c0136772f4
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2025-11-20 20:04:52 -0500

    async, await

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
+const myUsers = {
+  userList: [],
+};
+
+const myCoolFunction = async () => {
+  const jsonUserData = (
+    await fetch("https://jsonplaceholder.typicode.com/users")
+  ).json();
+
+  console.log(jsonUserData);
+};
+
 const users = fetch("https://jsonplaceholder.typicode.com/users")
   .then((response) => {
     return response.json();
   })
   .then((data) => {
-    data.forEach((element) => {
-      console.log(element);
+    data.forEach((user) => {
+      console.log(user);
     });
   });
-
-console.log(users);
